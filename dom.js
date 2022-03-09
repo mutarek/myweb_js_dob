0b5a8b103f51c0b73fa8384399f466c79951f520
DCL <bangladeshtourist@gmail.com>
2022-03-09 12:13:35 +0600

    dom

---
+++ dom.js
@@ -0,0 +1,15 @@
+// const maindv = document.getElementsByClassName("maindiv");
+
+// maindv.getElementsByTagName("h1").innerText = "Hello WOrld";
+
+// var roottag = document.getElementsByTagName("h1")[1].innerText = "My New Web";
+
+// console.log(roottag);
+
+function nextDesign() {
+    document.getElementById('product').setAttribute("class","changeproduct");
+}
+
+function previousDesign() {
+    document.getElementById('product').setAttribute("class","product");
+}
--- script.js
+++ script.js
@@ -1,16 +1,23 @@
-function showCustom() {
-    document.getElementById("author_layout").setAttribute("class","customauthor");
-    document.getElementById("authorh1").innerText = "Mohiuddin Tarek";
-}
+// function showCustom() {
+//     document.getElementById("author_layout").setAttribute("class","customauthor");
+//     document.getElementById("authorh1").innerText = "Mohiuddin Tarek";
+// }
 
-function showOrginal() {
-    document.getElementById("author_layout").setAttribute("class","author");
-    document.getElementById("authorh1").innerText = "Tofayel Ahmed";
-}
+// function showOrginal() {
+//     document.getElementById("author_layout").setAttribute("class","author");
+//     document.getElementById("authorh1").innerText = "Tofayel Ahmed";
+// }
 
 var element = document.getElementsByClassName('author').innerHTML = "Mohiuddin Tarek";
-console.log(element.innerHTML);
 
+const maindiv = document.getElementById("test");
+
+maindiv.getElementsByTagName("p").innerHTML = "Hello Jerin";
+
+var x = document.getElementById("test");
+var y = x.getElementsByTagName("p").innerHTML = "Hello Mahmood";
+
+document.write(y);
 
 
 
